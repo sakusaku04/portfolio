13a23c4c766f59e508b401caf483bafc695ee449
sakusaku04 <153100628+sakusaku04@users.noreply.github.com>
2024-01-03 22:48:34 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$('.main-nav a[href*="#"]').click(function () {
+  var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+  var pos = $(elmHash).offset().top;  //idの上部の距離を取得
+  $('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+  return false;
+});
+
+function toggleNav() {
+  const body = document.body;
+  const hamburger = document.getElementById('js-hamburger');
+  const blackBg = document.getElementById('js-black-bg');
+
+  hamburger.addEventListener('click', function() {
+    body.classList.toggle('nav-open');
+  });
+  blackBg.addEventListener('click', function() {
+    body.classList.remove('nav-open');
+  });
+}
+toggleNav();
